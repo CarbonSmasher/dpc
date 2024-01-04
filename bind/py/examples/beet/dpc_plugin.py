0aebfded55c6f5d28bf3421f66328405d9d2e6b7
CarbonSmasher <crabonsmasher@gmail.com>
2024-01-04 15:09:58 -0500

    Fix

--- bind/py/examples/beet/dpc_plugin.py
+++ bind/py/examples/beet/dpc_plugin.py
@@ -3,7 +3,7 @@ import datapack_compiler as dpc
 from beet import Context, TextFile
 
 def beet_default(ctx: Context):
-	ctx.data.extend_namespace.append(IRFile)
+  ctx.data.extend_namespace.append(IRFile)
 
 class IRFile(TextFile):
   scope: ClassVar[tuple[str, ...]] = ("ir",)
