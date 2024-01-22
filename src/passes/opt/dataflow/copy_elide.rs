725875fe0be6be2afcc9f8852590ddc12967d766
CarbonSmasher <crabonsmasher@gmail.com>
2024-01-22 09:12:40 -0500

    Call arg copy elision

--- src/passes/opt/dataflow/copy_elide.rs
+++ src/passes/opt/dataflow/copy_elide.rs
@@ -1,12 +1,15 @@
 use std::cell::RefCell;
 
+use intset::GrowSet;
 use rustc_hash::{FxHashMap, FxHashSet};
 
+use crate::common::block::Block;
 use crate::common::val::ScoreValue;
 use crate::common::{val::MutableScoreValue, Identifier};
 use crate::lir::{LIRBlock, LIRInstrKind};
 use crate::passes::{LIRPass, LIRPassData, Pass};
 use crate::project::{OptimizationLevel, ProjectSettings};
+use crate::util::remove_indices;
 
 pub struct CopyElisionPass;
 
@@ -25,24 +28,33 @@ impl LIRPass for CopyElisionPass {
 		let mut arg_mapping = FxHashMap::default();
 		let mut used_args = FxHashSet::default();
 		let mut call_ret_mapping = FxHashMap::default();
+		let mut call_arg_mapping = FxHashMap::default();
 
 		for func in data.lir.functions.values_mut() {
 			let block = &mut func.block;
+
+			let mut instrs_to_remove = block.get_index_set();
 
 			loop {
 				arg_mapping.clear();
 				used_args.clear();
 				call_ret_mapping.clear();
+				call_arg_mapping.clear();
+
 				let run_again = run_iter(
 					block,
 					&mut arg_mapping,
 					&mut used_args,
 					&mut call_ret_mapping,
+					&mut call_arg_mapping,
+					&mut instrs_to_remove,
 				);
 				if !run_again {
 					break;
 				}
 			}
+
+			remove_indices(&mut block.contents, &instrs_to_remove);
 		}
 
 		Ok(())
@@ -54,9 +66,12 @@ fn run_iter(
 	arg_mapping: &mut FxHashMap<Identifier, MutableScoreValue>,
 	used_args: &mut FxHashSet<usize>,
 	call_ret_mapping: &mut FxHashMap<Identifier, MutableScoreValue>,
+	call_arg_mapping: &mut FxHashMap<Identifier, MutableScoreValue>,
+	instrs_to_remove: &mut GrowSet,
 ) -> bool {
 	let mut run_again = false;
 
+	// Run the forward propagating elisions
 	for instr in &mut block.contents.iter_mut() {
 		match &instr.kind {
 			LIRInstrKind::SetScore(
@@ -106,6 +121,42 @@ fn run_iter(
 				if let Some(call_ret) = call_ret_mapping.get(reg) {
 					*val = call_ret.clone();
 					*run_again_2.borrow_mut() = true;
+				}
+			}
+		});
+		run_again = run_again_2.take();
+	}
+
+	// Run the backward propagating elisions
+	for (i, instr) in &mut block.contents.iter_mut().enumerate().rev() {
+		if instrs_to_remove.contains(i) {
+			continue;
+		}
+
+		match &instr.kind {
+			LIRInstrKind::SetScore(
+				l @ MutableScoreValue::CallArg(..),
+				ScoreValue::Mutable(MutableScoreValue::Reg(r)),
+			) => {
+				call_arg_mapping.insert(r.clone(), l.clone());
+				// We have to remove this assignment since it is now assigning
+				// to an invalid register
+				instrs_to_remove.add(i);
+				// We don't want to create weird assign arg to self instructions, continue
+				continue;
+			}
+			_ => {}
+		}
+
+		// Replace the uses of registers that are args or call rets
+		// with the values themselves
+		let run_again_2 = RefCell::new(false); // Closure thing
+		instr.replace_mut_score_vals(&|val| {
+			if let MutableScoreValue::Reg(reg) = val {
+				if let Some(call_arg) = call_arg_mapping.get(reg) {
+					*val = call_arg.clone();
+					*run_again_2.borrow_mut() = true;
+					return;
 				}
 			}
 		});
--- test/codegen/tests/main/func/call.mcfunction
+++ test/codegen/tests/main/func/call.mcfunction
@@ -1,14 +1,26 @@
 # === dpc:init === #
 scoreboard objectives add _r dummy
+scoreboard objectives add _l dummy
+scoreboard players set %l3 _l 3
+scoreboard players set %l6 _l 6
+scoreboard players set %l10 _l 10
 
 # === test:fn === #
 scoreboard players operation %atest_fn.0 _r *= %atest_fn.1 _r
 data modify entity @s DisplayName set from storage dpc:r atest_fn_2
 
+# === test:fn2 === #
+scoreboard players operation %rtest_fn2.0 _r = %atest_fn2.0 _r
+scoreboard players add %atest_fn2.0 _r 3
+scoreboard players operation %atest_fn2.0 _r /= %l3 _l
+
 # === test:main === #
-scoreboard players set %rtest_main.0 _r -44
+scoreboard players set %atest_fn.1 _r -44
 scoreboard players set %atest_fn.0 _r 7
-scoreboard players operation %atest_fn.1 _r = %rtest_main.0 _r
 data modify storage dpc:r atest_fn_2 set value "foo"
 function test:fn
 scoreboard players operation %rtest_main.0 _r = %Rtest_fn.0 _r
+scoreboard players operation %atest_fn2.0 _r = @s foo
+scoreboard players operation %atest_fn2.0 _r *= %l10 _l
+scoreboard players operation %atest_fn2.0 _r %= %l6 _l
+function test:fn2
